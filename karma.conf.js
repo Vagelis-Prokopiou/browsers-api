703006022097f420354e715f18dda49ae0d7240f
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2017-07-12 15:03:59 +0300

    config
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
   config.set({
     basePath: './',
     frameworks: ['jasmine'],
-    browsers: ['Chrome', 'Firefox']
+    browsers: ['Chrome', 'Firefox'],
+    files: ['tests.js']
   });
 };
