888555a2224e009fdeeb1b76c8c83143697bc2fa
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2017-07-17 14:03:57 +0300

    Array.from()
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- tests.js
+++ tests.js
@@ -1,9 +1,29 @@
 describe("Array", function() {
 	
-	describe("Array.length", function() {
-	  it("should return the right array length", function() {
-	    expect([1,2,3].length).toBe(3);
+	describe("Array.length", () => 
+	{
+	  it("should return the right array length", () =>
+	  {
+	    expect([1,2,3].length).toEqual(3);
 	  });
-	});  
+	});
+
+	describe("Array.from()", () => 
+	{
+		function f() {
+		  return Array.from(arguments);
+		}
+
+		it("should  creates a new Array instance from an array-like or iterable object", () =>
+		{
+		  expect(Array.from(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+		  expect(Array.from('foo')).toEqual(["f", "o", "o"]);
+		  expect(Array.from(new Set(['foo', window]))).toEqual(['foo', window]);
+		  expect(Array.from(new Map([[1, 2], [2, 4], [4, 8]]))).toEqual([[1, 2], [2, 4], [4, 8]]);
+		  expect(f(1, 2, 3)).toEqual([1,2,3]);
+		  expect(Array.from([1, 2, 3], x => x + x)).toEqual([2,4,6]);
+		  expect(Array.from({length: 5}, (v, i) => i)).toEqual([0, 1, 2, 3, 4]);
+		});
+	});
 
 });
