d7c279c477e6d4ef8f68c04d441887b0df78d7cd
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2017-07-17 15:31:31 +0300

    Array.prototype.every()
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- tests.js
+++ tests.js
@@ -10,19 +10,20 @@ describe("Array", function() {
 
 	describe("Array.from()", () => 
 	{
-		function f() {
-		  return Array.from(arguments);
-		}
-
 		it("should create a new Array instance from an array-like or iterable object", () =>
 		{
-		  expect(Array.from(["a", "b", "c"])).toEqual(["a", "b", "c"]);
-		  expect(Array.from('foo')).toEqual(["f", "o", "o"]);
-		  expect(Array.from(new Set(['foo', window]))).toEqual(['foo', window]);
-		  expect(Array.from(new Map([[1, 2], [2, 4], [4, 8]]))).toEqual([[1, 2], [2, 4], [4, 8]]);
-		  expect(f(1, 2, 3)).toEqual([1,2,3]);
-		  expect(Array.from([1, 2, 3], x => x + x)).toEqual([2,4,6]);
-		  expect(Array.from({length: 5}, (v, i) => i)).toEqual([0, 1, 2, 3, 4]);
+			function f() 
+			{
+			  return Array.from(arguments);
+			}
+
+			expect(Array.from(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+			expect(Array.from('foo')).toEqual(["f", "o", "o"]);
+			expect(Array.from(new Set(['foo', window]))).toEqual(['foo', window]);
+			expect(Array.from(new Map([[1, 2], [2, 4], [4, 8]]))).toEqual([[1, 2], [2, 4], [4, 8]]);
+			expect(f(1, 2, 3)).toEqual([1,2,3]);
+			expect(Array.from([1, 2, 3], x => x + x)).toEqual([2,4,6]);
+			expect(Array.from({length: 5}, (v, i) => i)).toEqual([0, 1, 2, 3, 4]);
 		});
 	});
 
@@ -30,11 +31,11 @@ describe("Array", function() {
 	{
 		it("should determine whether the passed value is an Array", () =>
 		{
-		  expect(Array.isArray([1, 2, 3])).toEqual(true);
-		  expect(Array.isArray({foo: 123})).toEqual(false);
-		  expect(Array.isArray('foobar')).toEqual(false);
-		  expect(Array.isArray(undefined)).toEqual(false);
-		  expect(Array.isArray({ __proto__: Array.prototype })).toEqual(false);
+			expect(Array.isArray([1, 2, 3])).toEqual(true);
+			expect(Array.isArray({foo: 123})).toEqual(false);
+			expect(Array.isArray('foobar')).toEqual(false);
+			expect(Array.isArray(undefined)).toEqual(false);
+			expect(Array.isArray({ __proto__: Array.prototype })).toEqual(false);
 		});
 	});
 
@@ -42,9 +43,9 @@ describe("Array", function() {
 	{
 		it("should create a new Array instance with a variable number of arguments, regardless of number or type of the arguments", () =>
 		{
-		  expect(Array.of(1)).toEqual([1]);
-		  expect(Array.of(1, 2, 3)).toEqual([1, 2, 3]);
-		  expect(Array.of(undefined)).toEqual([undefined]);
+			expect(Array.of(1)).toEqual([1]);
+			expect(Array.of(1, 2, 3)).toEqual([1, 2, 3]);
+			expect(Array.of(undefined)).toEqual([undefined]);
 		});
 	});
 
@@ -88,6 +89,20 @@ describe("Array", function() {
 			expect(iterator.next().value).toEqual([0, 'a']);
 			expect(iterator.next().value).toEqual([1, 'b']);
 			expect(iterator.next().value).toEqual([2, 'c']);
+		});
+	});
+
+	describe("Array.prototype.every()", () => 
+	{
+		it("should test whether all elements in the array pass the test implemented by the provided function.", () =>
+		{
+			function isBigEnough(element, index, array)
+			{
+			  return element >= 10;
+			}
+
+			expect([12, 5, 8, 130, 44].every(isBigEnough)).toEqual(false);
+			expect([12, 54, 18, 130, 44].every(isBigEnough)).toEqual(true);
 		});
 	});
 
