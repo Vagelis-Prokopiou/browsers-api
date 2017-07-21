c9eda53a6c9515b998f17137ecc0865e7c25e75f
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2017-07-21 11:24:41 +0300

    Adding code coverage
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,21 @@
 // karma.conf.js
-module.exports = function(config) {
+module.exports = function(config)
+{
   config.set({
     basePath: './',
     frameworks: ['jasmine'],
     browsers: ['Chrome', 'Firefox'],
-    files: ['tests.js']
+    files: ['tests.js'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      // source files, that you wanna generate coverage for 
+      // do not include tests or libraries 
+      // (these files will be instrumented by Istanbul) 
+      'tests.js': ['coverage']
+    },
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    }
   });
 };
